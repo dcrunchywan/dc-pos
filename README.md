a31fe5f236821016dbc657cdfc0a3d048bcfdda7
dcrunchywan <dcrunchywan@gmail.com>
2026-07-09 03:25:21 +0700

    chore: clean vite template

---
+++ README.md
@@ -0,0 +1,7 @@
+# D'CrunchyWan POS v3
+
+Offline First Point of Sale.
+
+Status:
+
+🚧 Sprint 1 Development
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+<div id="app-shell">
+    <h1>D'CrunchyWan POS v3</h1>
+</div>
+`
